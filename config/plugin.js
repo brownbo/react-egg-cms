173cd805c927149a23e35110ca141610b9619449
mrliang <619727242@qq.com>
2018-09-14 10:52:10 +0800

    init

---
+++ config/plugin.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// had enabled by egg
+exports.static = true;
+
+// pug
+exports.pug = {
+  enable: true,
+  package: 'egg-view-pug'
+};
+
+// sequelize
+exports.sequelize = {
+  enable: true,
+  package: 'egg-sequelize'
+};
